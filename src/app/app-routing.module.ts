8af5005ee49cbbb74d58724498759bc7f877c70b
devhabib <habibr152@gmail.com>
2023-07-08 01:49:21 +0600

    added components for video

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { VideoWatchComponent } from './components/video-watch/video-watch.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-
+  { path: '', redirectTo: 'videos', pathMatch: 'full' },
+  { path: 'videos', component: VideoListComponent },
+  { path: 'videos/:id', component: VideoWatchComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { LayoutsModule } from './layouts/layouts.module';
 import { MainComponent } from './components/main/main.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { VideoWatchComponent } from './components/video-watch/video-watch.component';
+import { VideoListComponent } from './components/video-list/video-list.component';
+import { VideoSearchComponent } from './components/video-search/video-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    VideoWatchComponent,
+    VideoListComponent,
+    VideoSearchComponent
   ],
   imports: [
     BrowserModule,
