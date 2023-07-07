d68dcd5c6920a8a231799e19d05615d1a98d052e
devhabib <habibr152@gmail.com>
2023-07-07 22:51:51 +0600

    api service created

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private _http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this._http.get('https://gist.githubusercontent.com/poudyalanil/ca84582cbeb4fc123a13290a586da925/raw/14a27bd0bcd0cd323b35ad79cf3b493dddf6216b/videos.json')
+  }
+}
